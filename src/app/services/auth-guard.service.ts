987b9cfa8b779298b9aaffb71f5c3ce4e6ab137b
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-13 17:06:54 +0200

    auth guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanActivate {
 
-  constructor() { }
+  auth: Observable<firebase.User>;
+
+  constructor(
+    public router: Router,
+    public afAuth: AngularFireAuth,
+  ) {
+    this.auth = afAuth.authState;
+  }
+
+  canActivate(): Promise<boolean> {
+    return new Promise(Resolve => {
+      this.auth.subscribe(state => {
+        if (state) {
+          return Resolve(true);
+        } else {
+          this.router.navigate(['/login']);
+          return Resolve(false);
+        }
+      });
+    });
+  }
 }
