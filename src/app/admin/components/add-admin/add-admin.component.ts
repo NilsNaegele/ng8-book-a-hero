ecc8b7ed48d327868942991910a9bdb792a03b4b
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-13 20:11:06 +0200

    add-admin cmp

--- src/app/admin/components/add-admin/add-admin.component.ts
+++ src/app/admin/components/add-admin/add-admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { GlobalService } from 'src/app/services/global.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-admin',
@@ -6,10 +11,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-admin.component.scss']
 })
 export class AddAdminComponent implements OnInit {
+  newEmail: string;
+  newRole: string;
+  currentAdmin: Observable<any>;
+  editMode: boolean;
+  adminKey: string;
 
-  constructor() { }
+  constructor(
+    public db: AngularFireDatabase,
+    public snackBar: MatSnackBar,
+    public router: Router,
+    public route: ActivatedRoute,
+    public globalService: GlobalService
+  ) {
+    this.editMode = false;
+  }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+        if (params && params.key) {
+          this.editMode = true;
+          this.adminKey = params.key;
+          this.currentAdmin = this.db.object('/admins/' + params.key).valueChanges();
+
+          this.currentAdmin.subscribe((a: any) => {
+            this.newEmail = a.email;
+            this.newRole = a.role;
+          });
+        } else {
+          this.newEmail = null;
+          this.newRole = 'editor';
+        }
+    });
+  }
+
+  addAdmin(newEmail: string, newRole: string): void {
+    if (newEmail && newRole) {
+
+      this.db.object('/admins/' + this.globalService.hashCode(newEmail)).update({
+        email: newEmail,
+        role: newRole
+      });
+
+      this.newEmail = null;
+      this.newRole = null;
+
+      const snackBarRef = this.snackBar.open('Admin saved', 'OK!', {
+        duration: 3000
+      });
+
+    } else if (!newEmail) {
+      const snackBarRef = this.snackBar.open('You must add an email for the user', 'OK!', {
+        duration: 3000
+      });
+    } else if (!newRole) {
+      const snackBarRef = this.snackBar.open('You must add a role for the user', 'OK!', {
+        duration: 3000
+      });
+    }
   }
 
 }
