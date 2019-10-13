3ccbff97f3b2bdb005b73278e90579ad9069a2fe
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-13 20:31:53 +0200

    add order cmp

--- src/app/admin/components/add-order/add-order.component.ts
+++ src/app/admin/components/add-order/add-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { GlobalService } from 'src/app/services/global.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-order',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddOrderComponent implements OnInit {
 
-  constructor() { }
+  orders: AngularFireList<any>;
+  order: any;
+  editMode: boolean;
+  orderKey: string;
+  countries: any;
+  statuses: Array<any>;
+  users: AngularFireList<any>;
+
+  constructor(
+    public db: AngularFireDatabase,
+    public snackBar: MatSnackBar,
+    public globalService: GlobalService,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {
+    this.countries = globalService.countries;
+    this.statuses = globalService.orderStatuses;
+    this.orders = db.list('/orders');
+    this.users = db.list('/users');
+    this.order = { shipping: {}, billing: {} };
+  }
+
+  addOrder(newOrder): void {
+    if (newOrder) {
+      if (this.editMode && this.orderKey) {
+        this.db.object('/orders/' + this.orderKey).update(newOrder);
+      } else {
+        this.orders.push(newOrder);
+      }
+
+      const snackBarRef = this.snackBar.open('Order saved', 'OK!', {
+        duration: 3000
+      });
+    }
+  }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+        if (params && params.key) {
+          this.editMode = true;
+          this.orderKey = params.key;
+          this.db.object('/orders/' + params.key).valueChanges().subscribe((o: any) => {
+            this.order = o;
+          });
+        } else {
+          this.order = { shipping: {}, billing: {} };
+        }
+    });
   }
 
 }
