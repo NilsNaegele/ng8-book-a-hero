3b603168439384941e57aca15db84b4c2a9663ef
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-13 15:48:00 +0200

    sort pipe

--- src/app/shared/pipes/sort.pipe.ts
+++ src/app/shared/pipes/sort.pipe.ts
@@ -15,8 +15,49 @@ export class SortPipe implements PipeTransform {
     }
   }
 
-  transform(value: any, ...args: any[]): any {
-    return null;
-  }
+  transform(input: any, [config = '+']): any {
+
+    if (!Array.isArray(input)) {
+      return input;
+    }
+
+    if (!Array.isArray(config) || (Array.isArray(config) && config.length === 1)) {
+        const propertyToCheck = !Array.isArray(config) ? config : config[0];
+        const desc = propertyToCheck.substr(0, 1) === '-';
+
+
+        if ( !propertyToCheck || propertyToCheck === '-' || propertyToCheck === '+') {
+            return !desc ? input.sort() : input.sort().reverse();
+        } else {
+            const property = propertyToCheck.substr(0, 1) === '+' || propertyToCheck.substr(0, 1) === '-'
+                ? propertyToCheck.substr(1)
+                : propertyToCheck;
+
+            return input.sort((a: any, b: any) => {
+                return !desc
+                    ? SortPipe.orderByComparator(a[property], b[property])
+                    : -SortPipe.orderByComparator(a[property], b[property]);
+            });
+        }
+    } else {
+        return input.sort((a: any, b: any) => {
+            // tslint:disable-next-line: prefer-for-of
+            for (let i = 0; i < config.length; i++) {
+                const desc = config[i].substr(0, 1) === '-';
+                const property = config[i].substr(0, 1) === '+' || config[i].substr(0, 1) === '-'
+                    ? config[i].substr(1)
+                    : config[i];
+
+                const comparison = !desc
+                    ? SortPipe.orderByComparator(a[property], b[property])
+                    : -SortPipe.orderByComparator(a[property], b[property]);
+                if (comparison !== 0) {
+                  return comparison;
+                }
+            }
+            return 0;
+        });
+    }
+}
 
 }
