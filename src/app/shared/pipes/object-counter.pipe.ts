69f049b85fe372b28bfd086c9b3204a814b37c71
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-13 16:00:27 +0200

    object count pipe

--- src/app/shared/pipes/object-counter.pipe.ts
+++ src/app/shared/pipes/object-counter.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ObjectCounterPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, args?: any): any {
+    return (value) ? Object.keys(value).length : 0;
   }
 
 }
