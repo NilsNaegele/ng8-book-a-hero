f5864c7339b6d96f5c10e6affc4ec1d9dbd9af65
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-19 17:50:43 +0200

    admin-clients-cmp

--- src/app/admin/components/admin-clients/admin-clients.component.ts
+++ src/app/admin/components/admin-clients/admin-clients.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GlobalService } from 'src/app/services/global.service';
+import { AdminDeleteDialogComponent } from '../admin-delete-dialog/admin-delete-dialog.component';
 
 @Component({
   selector: 'app-admin-clients',
   templateUrl: './admin-clients.component.html',
   styleUrls: ['./admin-clients.component.scss']
 })
-export class AdminClientsComponent implements OnInit {
+export class AdminClientsComponent {
 
-  constructor() { }
+  customers: Observable<any[]>;
+  selectedOption: any;
+  dialogRef: MatDialogRef<any>;
+  currentAdmin: any;
 
-  ngOnInit() {
+  constructor(
+    public db: AngularFireDatabase,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public globalService: GlobalService
+  ) {
+    this.customers = db.list('/users').snapshotChanges();
+
+    this.globalService.admin.subscribe((a) => {
+      this.currentAdmin = a;
+    });
   }
 
+  deleteCustomer(event, key: string) {
+    event.stopPropagation();
+    const dialogRef = this.dialog.open(AdminDeleteDialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.selectedOption = result;
+      if (this.selectedOption === 'delete') {
+        this.db.object('/users/' + key).remove();
+
+        const snackBarRef = this.snackBar.open('Customer deleted', 'OK!', {
+          duration: 3000
+        });
+      }
+    });
+  }
 }
