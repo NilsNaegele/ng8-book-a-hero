96c959ccfeaf54ff733eb1c464dd7013aedc65f7
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-19 18:54:02 +0200

    admin cmp

--- src/app/admin/components/admin/admin.component.ts
+++ src/app/admin/components/admin/admin.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { GlobalService } from 'src/app/services/global.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+  currentAdmin: any;
+
+  constructor(
+    public db: AngularFireDatabase,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public globalService: GlobalService,
+    public snackBar: MatSnackBar,
+  ) {
+    this.user = afAuth.authState;
+    this.currentAdmin = {};
+  }
 
   ngOnInit() {
+    this.user.subscribe(currentUser => {
+      if (!currentUser) {
+        this.router.navigateByUrl('login');
+      } else {
+        this.db.object('/admins/' + this.globalService.hashCode(currentUser.email)).valueChanges().subscribe((a: any) => {
+          if (a && a.email) {
+            this.globalService.admin.next(currentUser);
+            this.db.object('/admins/' + currentUser.uid).valueChanges().subscribe((b: any) => {
+              this.globalService.admin.next(b);
+              this.currentAdmin.role = b.role;
+            });
+          } else {
+            this.router.navigateByUrl('');
+            const snackBarRef = this.snackBar.open('You are not an authorized administrator', 'OK!', {
+              duration: 3000
+            });
+          }
+        }, (err) => {
+          console.log('Permission Error', err);
+          this.router.navigateByUrl('');
+          const snackBarRef = this.snackBar.open('You are not an authorized administrator', 'OK!', {
+            duration: 3000
+          });
+        });
+      }
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
