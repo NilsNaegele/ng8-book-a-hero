855d64a6f4640f7257c23848a542e3f0f7a1d5eb
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-19 18:56:09 +0200

    approve dlg cmp

--- src/app/admin/components/admin-approve-dialog/admin-approve-dialog.component.ts
+++ src/app/admin/components/admin-approve-dialog/admin-approve-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-approve-dialog',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminApproveDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<any>) { }
 
   ngOnInit() {
   }
