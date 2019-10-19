e7e61e3131cd081a8d20c7b3c8a1ef0a4a1ec7dd
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-19 18:46:10 +0200

    admin-theme cmp

--- src/app/admin/components/admin-theme/admin-theme.component.ts
+++ src/app/admin/components/admin-theme/admin-theme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireObject, AngularFireDatabase } from 'angularfire2/database';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin-theme',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminThemeComponent implements OnInit {
 
-  constructor() { }
+  theme: AngularFireObject<any>;
+  siteName: string;
+
+  constructor(public db: AngularFireDatabase, public snackBar: MatSnackBar) {
+    this.theme = db.object('/theme');
+  }
 
   ngOnInit() {
+    this.theme.valueChanges().subscribe((item: any) => {
+      if (item && item.siteName) {
+        this.siteName = item.siteName;
+      }
+    });
+  }
+
+  saveTheme(newSiteName: string) {
+    this.theme.update({
+      siteName: newSiteName
+    });
+
+    const snackBarRef = this.snackBar.open('Theme updated', 'OK!', {
+      duration: 3000
+    });
   }
 
 }
